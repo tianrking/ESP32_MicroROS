8aa44491d3ac24abac6c077814f5409c12089498
w0x7ce <tian.r.king@gmail.com>
2023-01-11 21:13:59 +0800

    Update: gpio

--- main/diy_esp32c3.c
+++ main/diy_esp32c3.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#define D4 2
 
 void app_main(void)
 {
     char *ourTaskName = pcTaskGetName(NULL);
     ESP_LOGI(ourTaskName,"GG");
 
+    gpio_reset_pin(D4);
+    gpio_set_direction(D4,GPIO_MODE_OUTPUT);
+
     while(1){
-        ;;
+        gpio_set_level(D4,1);
+        vTaskDelay( 1000 / portTICK_PERIOD_MS);
+        gpio_set_level(D4,0);
+        vTaskDelay( 1000 / portTICK_PERIOD_MS);
     }
 }
