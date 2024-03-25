c7f845ae2225244a8efb0d6626df597dab3f1ab3
w0x7ce <tian.r.king@gmail.com>
2024-03-25 18:21:46 +0800

    On branch wit_jy_901s_imu FIX microros task  Changes to be committed: 	modified:   main/main.c

--- main/main.c
+++ main/main.c
@@ -6,12 +6,25 @@
 #include "freertos/task.h"
 #include "wit_task.h"
 #include "microros.h"
+#include <unistd.h> 
+#include <uros_network_interfaces.h>
 
 void app_main(void)
 {
 	xTaskCreate(Usart0_task, "Usart0_task", 4096, NULL, 5, NULL);
 	xTaskCreate(Usart1_task, "Usart1_task", 4096, NULL, 5, NULL);
-    xTaskCreate(micro_ros_task, "Usart1_task", 4096, NULL, 5, NULL);
+
+    #if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
+            ESP_ERROR_CHECK(uros_network_interface_initialize());
+    #endif
+    //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
+    xTaskCreate(micro_ros_task,
+            "uros_task",
+            CONFIG_MICRO_ROS_APP_STACK,
+            NULL,
+            CONFIG_MICRO_ROS_APP_TASK_PRIO,
+            NULL);
+
 
 	wit_init_all();
 	while (1)
